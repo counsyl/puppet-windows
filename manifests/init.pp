9e84391c25205fab07c9efbd28ddf14b95d110b2
Justin Bronn <justin@counsyl.com>
2013-10-21 11:39:00 -0700

    Added some common variables and a platform check.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,16 @@
 #
 class windows(
   $installers = 'C:\ProgramData\installers',
+  $system32   = 'C:\WINDOWS\system32',
 ){
+  if $::osfamily != 'windows' {
+    fail("The windows module is only supported on Microsoft Windows.\n")
+  }
+
+  # Location of commonly-used programs from system32.
+  $certutil = "${system32}\\certutil.exe"
+  $cmd = "${system32}\\cmd.exe"
+
   file { $installers:
     ensure => directory,
   }
