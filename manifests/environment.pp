b13b30ea00d0fb5e472c5ac7651091acc121577f
Justin Bronn <justin@counsyl.com>
2013-11-10 14:40:55 -0800

    No longer accept 'Process' as a target (doesn't make sense); use double quotes because single ones are allowed in paths.

--- manifests/environment.pp
+++ manifests/environment.pp
@@ -18,7 +18,7 @@
 #
 # [*target*]
 #  The location where an environment variable is stored, must be either
-#  'Machine' (the default), 'Process', or 'User'.
+#  'Machine' (the default) or 'User'.
 #
 define windows::environment(
   $value    = undef,
@@ -28,19 +28,19 @@ define windows::environment(
 ) {
 
   # Ensure only valid target parameter.
-  validate_re($target, '^(Machine|Process|User)$', 'Invalid target parameter')
+  validate_re($target, '^(Machine|User)$', 'Invalid target parameter')
 
   case $ensure {
     'present': {
       if (! $value) {
         fail("Must provide a value to set ${variable} with.\n")
       }
-      $command = "[Environment]::SetEnvironmentVariable('${variable}', '${value}', '${target}')"
-      $unless = "if ([Environment]::GetEnvironmentVariable('${variable}', '${target}') -ne '${value}'){ exit 1 }"
+      $command = "[Environment]::SetEnvironmentVariable(\"${variable}\", \"${value}\", \"${target}\")"
+      $unless = "if ([Environment]::GetEnvironmentVariable(\"${variable}\", \"${target}\") -ne \"${value}\"){ exit 1 }"
     }
     'absent': {
-      $command = "[Environment]::SetEnvironmentVariable('${variable}', \$null, '${target}')"
-      $unless = "if ([Environment]::GetEnvironmentVariable('${variable}', '${target}') -ne \$null){ exit 1 }"
+      $command = "[Environment]::SetEnvironmentVariable(\"${variable}\", \$null, \"${target}\")"
+      $unless = "if ([Environment]::GetEnvironmentVariable(\"${variable}\", \"${target}\") -ne \$null){ exit 1 }"
     }
     default: {
       fail("Invalid windows::environment ensure value.\n")
