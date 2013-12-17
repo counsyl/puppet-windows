c72cde188fe8bd73a93fe5db06181456956c08c3
Justin Bronn <justin@counsyl.com>
2013-12-16 16:45:09 -0800

    Enhanced docstrings, and added `refreshonly`, `unless`, `command_template`, and `provider` parameters.

--- manifests/unzip.pp
+++ manifests/unzip.pp
@@ -8,19 +8,46 @@
 #  Required, the destination directory to extract the files into.
 #
 # [*creates*]
-#  Required, a file that should exist after the ZIP file is extracted.
+#  The `creates` parameter for the exec resource that extracts the ZIP file,
+#  default is undefined.
+#
+# [*refreshonly*]
+#  The `refreshonly` parameter for the exec resource that extracts the ZIP file,
+#  defaults to false.
+#
+# [*unless*]
+#  The `unless` parameter for the exec resource that extracts the ZIP file,
+#  default is undefined.
 #
 # [*zipfile*]
 #  The path to the ZIP file to extract, defaults the $name of the resource.
 #
+# [*command_template*]
+#  Advanced paramter for generating PowerShell that extracts the ZIP file,
+#  defaults to 'windows/unzip.ps1.erb'.
+#
+# [*provider*]
+#  Advanced parameter, sets the provider for the exec resource that extracts
+#  the ZIP file, defaults to 'powershell'.
+#
 define windows::unzip(
   $destination,
-  $creates,
-  $zipfile = $name,
+  $creates          = undef,
+  $refreshonly      = false,
+  $unless           = undef,
+  $zipfile          = $name,
+  $command_template = 'windows/unzip.ps1.erb',
+  $provider         = 'powershell',
 ) {
+  if (! $creates and ! $refreshonly and ! $unless){
+    fail("Must set one of creates, refreshonly, or unless parameters.\n")
+  }
+
   exec { "unzip-${name}":
-    command  => template('windows/unzip.ps1.erb'),
-    creates  => $creates,
-    provider => 'powershell',
+    command     => template($command_template),
+    creates     => $creates,
+    refreshonly => $refreshonly,
+    unless      => $unless,
+    provider    => $provider,
   }
 }
