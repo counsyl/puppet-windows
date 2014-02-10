11f83a6a31cc06e7ec15a233329a183e99433345
Justin Bronn <justin@counsyl.com>
2014-02-10 01:09:24 -0800

    Fleshed out docstrings; accept minute and hour suffixes on timeout parameter.

--- manifests/screen_saver.pp
+++ manifests/screen_saver.pp
@@ -1,20 +1,44 @@
-# === Define: windows::screen_saver
+# == Define: windows::screen_saver
 #
-# TODO.
+# Sets up screen saver settings for the user of the given name.
+#
+# === Parameters
+#
+# [*ensure*]
+#  Ensure value for this resource, defaults to 'present'.  Set to 'absent' or
+#  'disabled' to disable the screen saver for the user.
+#
+# [*user*]
+#  The user to set the screen saver for, defaults to the name of the resource.
+#
+# [*domain*]
+#  Domain the user belongs, default is undefined.
+#
+# [*exe*]
+#  The executable to use for the screen saver, defaults to:
+#  'C:\Windows\system32\scrnsave.scr' (the blank screen saver).
+#
+# [*secure*]
+#  Whether a password is required to unlock the screen saver, default is false.
+#
+# [*timeout*]
+#  How long until the screen saver activates, may be specified as
+#  seconds ('3600'), minutes ('60m'), or hours ('1h').  Default is
+#  '300' (5 minutes).
 #
 define windows::screen_saver(
-  $ensure  = 'enabled',
+  $ensure  = 'present',
   $user    = $name,
   $domain  = undef,
   $exe     = 'C:\Windows\system32\scrnsave.scr',
-  $timeout = 300,
   $secure  = false,
+  $timeout = 300,
 ) {
   validate_re($ensure, '^(present|enabled|absent|disabled)$')
   validate_string($user)
   validate_absolute_path($exe)
   validate_bool($secure)
-  validate_re($timeout, '^\d+$')
+  validate_re($timeout, '^\d+[mMhH]?$')
 
   if $ensure in ['present', 'enabled'] {
     $active_value = 1
@@ -26,6 +50,21 @@ define windows::screen_saver(
     $secure_value = 1
   } else {
     $secure_value = 0
+  }
+
+  if $timeout =~ /^(\d+)([MmHh])$/ {
+    case $2 {
+      'm': {
+        $timeout_minutes = $1
+        $timeout_seconds = inline_template("<%= Integer(@timeout_minutes) * 60 %>")
+      }
+      'h': {
+        $timeout_hours = $1
+        $timeout_seconds = inline_template("<%= Integer(@timeout_hours) * 3600) %>")
+      }
+    }
+  } else {
+    $timeout_seconds = $timeout
   }
 
   exec { "screen-saver-${user}":
