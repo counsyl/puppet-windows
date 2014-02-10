fb983ef044c83645d6c7e5343b804fc08cdaa7c7
Justin Bronn <justin@counsyl.com>
2014-02-10 00:42:53 -0800

    No longer use native method way, as only applies to session user.

--- manifests/screen_saver.pp
+++ manifests/screen_saver.pp
@@ -1,15 +1,33 @@
 # === Define: windows::screen_saver
 #
-# TODO. 
+# TODO.
 #
 define windows::screen_saver(
+  $ensure  = 'enabled',
   $user    = $name,
   $domain  = undef,
-  $ensure  = 'present',
   $exe     = 'C:\Windows\system32\scrnsave.scr',
-  $timeout = '300',
+  $timeout = 300,
   $secure  = false,
 ) {
+  validate_re($ensure, '^(present|enabled|absent|disabled)$')
+  validate_string($user)
+  validate_absolute_path($exe)
+  validate_bool($secure)
+  validate_int($timeout)
+
+  if $ensure in ['present', 'enabled'] {
+    $active_value = 1
+  } else {
+    $active_value = 0
+  }
+
+  if $secure {
+    $secure_value = 1
+  } else {
+    $secure_value = 0
+  }
+
   exec { "screen-saver-${user}":
     command  => template('windows/screen_saver_set.ps1.erb'),
     unless   => template('windows/screen_saver_check.ps1.erb'),
