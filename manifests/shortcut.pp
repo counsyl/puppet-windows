a17325ddd97f0959ff8cc78deb3111767e783266
Justin Bronn <justin@counsyl.com>
2014-02-27 19:46:40 -0800

    Validate parameters individually, instead of as an array.

--- manifests/shortcut.pp
+++ manifests/shortcut.pp
@@ -11,7 +11,8 @@ define windows::shortcut(
   $working_directory = undef,
 ) {
   validate_re($path, '\.[lL][nN][kK]$')
-  validate_absolute_path([$path, $target])
+  validate_absolute_path($path)
+  validate_absolute_path($target)
   exec { "windows-shortcut-${path}":
     command  => template('windows/shortcut.ps1.erb'),
     creates  => $path,
