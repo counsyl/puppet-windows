219af318301aa0f0243f27f2c8e06e380cb1b64d
krulls <stephan.krull@ecg-leipzig.de>
2016-06-08 07:55:32 +0200

    Issue #7: validate that "target" is a string

--- manifests/shortcut.pp
+++ manifests/shortcut.pp
@@ -13,6 +13,7 @@ define windows::shortcut(
 ) {
   validate_re($path, '\.[lL][nN][kK]$')
   validate_absolute_path($path)
+  validate_string($target)
   exec { "windows-shortcut-${path}":
     command  => template('windows/shortcut.ps1.erb'),
     creates  => $path,
